a91d05a4e4423375040efc8f4cd40a641544c268
elkhailihamza <elkhailihamza331@gmail.com>
2024-02-01 19:51:14 +0100

    changed the img location inside the of the card frames so that it can fit properly

--- youbook/resources/views/booklist.blade.php
+++ youbook/resources/views/booklist.blade.php
@@ -16,32 +16,33 @@
 
 <div class="container mt-5 d-flex justify-content-center flex-wrap gap-4">
     @foreach($books as $i => $book)
-    <div class="card" style="width: 225px;">
+    <div class="card" style="width: 325px;">
         <a href="{{ route('book.view', ['book' => $book]) }}" class="text-decoration-none text-dark">
             <img src="{{ $book->book_cover ? asset('storage/' . $book->book_cover) : url('img/thumbnail.png') }}"
-                class="card-img-top" alt="Book Cover">
+                class="card-img-top cropped-img" style="height: 200px;" alt="Book Cover">
             <div class="card-body">
                 <h5 class="card-title">{{ $book->title }}</h5>
-                <p class="card-text text-truncate" style="max-width: 200px;">{{ $book->description }}</p>
+                <p class="card-text text-truncate" style="max-width: 300px;">{{ $book->description }}</p>
             </div>
         </a>
         <div class="p-2 d-flex gap-1">
-            <form method="post" action="">
+            <form method="post" action="{{ route('book.destroy', ['book' => $book]) }}">
                 @csrf
                 @method('delete')
-                <button type="submit" class="btn btn-danger slick_btn"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                        viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round"
-                        stroke-linejoin="round">
+                <button type="submit" class="btn btn-danger d-flex justify-content-center p-1"><svg
+                        xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+                        stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
                         <path d="M21 4H8l-7 8 7 8h13a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2z"></path>
                         <line x1="18" y1="9" x2="12" y2="15"></line>
                         <line x1="12" y1="9" x2="18" y2="15"></line>
                     </svg></button>
             </form>
-            <a href="{{ route('book.edit', ['book' => $book]) }}" class="btn btn-success slick_btn"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                    viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round"
-                    stroke-linejoin="round">
-                    <polygon points="14 2 18 6 7 17 3 17 3 13 14 2"></polygon>
-                    <line x1="3" y1="22" x2="21" y2="22"></line>
+            <a href="{{ route('book.edit', ['book' => $book]) }}"
+                class="btn btn-success d-flex justify-content-center p-1"><svg xmlns="http://www.w3.org/2000/svg"
+                    width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="1.5"
+                    stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M20 14.66V20a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h5.34"></path>
+                    <polygon points="18 2 22 6 12 16 8 16 8 12 18 2"></polygon>
                 </svg></a>
         </div>
     </div>
