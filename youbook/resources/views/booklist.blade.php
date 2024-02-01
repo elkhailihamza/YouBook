89e07a9a34690633674ba9121ca4a4ec52d6eab0
elkhailihamza <elkhailihamza331@gmail.com>
2024-02-01 13:54:27 +0100

    you can now view books

--- youbook/resources/views/booklist.blade.php
+++ youbook/resources/views/booklist.blade.php
@@ -25,20 +25,24 @@
                 <p class="card-text text-truncate" style="max-width: 200px;">{{ $book->description }}</p>
             </div>
         </a>
-        <div class="p-2">
-            <button class="btn btn-danger"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                    viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round"
-                    stroke-linejoin="round">
-                    <path d="M21 4H8l-7 8 7 8h13a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2z"></path>
-                    <line x1="18" y1="9" x2="12" y2="15"></line>
-                    <line x1="12" y1="9" x2="18" y2="15"></line>
-                </svg></button>
-            <button class="btn btn-success"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+        <div class="p-2 d-flex gap-1">
+            <form method="post" action="">
+                @csrf
+                @method('delete')
+                <button type="submit" class="btn btn-danger slick_btn"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                        viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round"
+                        stroke-linejoin="round">
+                        <path d="M21 4H8l-7 8 7 8h13a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2z"></path>
+                        <line x1="18" y1="9" x2="12" y2="15"></line>
+                        <line x1="12" y1="9" x2="18" y2="15"></line>
+                    </svg></button>
+            </form>
+            <a href="{{ route('book.edit', ['book' => $book]) }}" class="btn btn-success slick_btn"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
                     viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round"
                     stroke-linejoin="round">
                     <polygon points="14 2 18 6 7 17 3 17 3 13 14 2"></polygon>
                     <line x1="3" y1="22" x2="21" y2="22"></line>
-                </svg></button>
+                </svg></a>
         </div>
     </div>
     @endforeach
